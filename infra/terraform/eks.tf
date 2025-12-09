f15f498160a3b896283417b664d9a8028cefb072
Pankaj Kumar Yadav <131384457+Pankaj8821@users.noreply.github.com>
2025-12-09 14:55:38 +0530

    Add security group rules for EKS cluster
    Added security group rules to allow ingress on port 8080 and egress traffic for the EKS cluster.

--- infra/terraform/eks.tf
+++ infra/terraform/eks.tf
@@ -111,3 +111,26 @@ resource "aws_eks_node_group" "ng" {
     aws_eks_cluster.eks
   ]
 }
+
+resource "aws_security_group_rule" "eks_allow_8080_from_internet" {
+  type              = "ingress"
+  description       = "Allow traffic to pods on port 8080 (for k8s Service type=LoadBalancer)"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_eks_cluster.eks.cluster_security_group_id
+}
+
+resource "aws_security_group_rule" "eks_allow_all_egress_explicit" {
+  type              = "egress"
+  description       = "Allow all outbound traffic from cluster SG (explicit)"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  ipv6_cidr_blocks  = ["::/0"]
+  security_group_id = aws_eks_cluster.eks.cluster_security_group_id
+}
+
